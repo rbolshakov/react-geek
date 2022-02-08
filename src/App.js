fb3623f42691df04286c4a3e306e0c3ee05a7f6c
Rodion Bolshakov <hi0nity@yandex.ru>
2022-02-08 11:14:49 +0300

    corrected lesson1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import Messages from './Components/Messages/Messages'
-import { BrowserRouter } from 'react-router-dom';
+import { Messages } from './Components/Messages/Messages'
 
-function App() {
+export function App() {
   const text = 'Some Text'
   return (
     <div className="App">
       <div className="app-wrapper">
-        <BrowserRouter>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-          <Messages text={text} />
-        </BrowserRouter>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Messages text={text} />
       </div>
-
     </div>
   );
 }
 
-export default App;
