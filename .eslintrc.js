98896820b7f6a45ce2bc84391ac4496d49abc7c7
Rodion Bolshakov <hi0nity@yandex.ru>
2022-02-08 09:19:53 +0300

    add .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,7 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:jsx-a11y/recommended",
-        "plugin:import/recommended"
+        "plugin:react/recommended"
     ],
     "parserOptions": {
         "ecmaFeatures": {
